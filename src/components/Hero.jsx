afe7a522e9890fe37f63c0815edd91e8779557d1
Fidan-Eyyubova <fidaneyybva0@gmail.com>
2025-10-25 13:50:40 +0400

    Hero navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,29 @@
 import { motion } from "framer-motion";
-
-// import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas/Computers";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto   bg-[url('herobg.png')] bg-cover bg-center bg-no-repeat`}>
+    <section
+      className={`relative w-full h-screen mx-auto   bg-[url('herobg.png')] bg-cover bg-center bg-no-repeat`}
+    >
       <div
         className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto !sm:px-16 !px-6 flex flex-row items-start gap-5`}
       >
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
 
         <div>
-          <h1 className={`text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]`}>
-            Hi, I'm <span className='text-[#915EFF]'>Fidan</span>
+          <h1
+            className={`text-[#dfd9ff] font-bold lg:text-[40px] sm:text-[26px] xs:text-[20px] text-[40px] lg:leading-[50px]`}
+          >
+            Hi, I'm <span className="text-[#915EFF]">Fidan</span>
           </h1>
-          <p className={`sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
+          <p
+            className={`!sm:text-[18px] !text-[25px] uppercase tracking-wider mt-5 `}
+          >
+            I develop 3D visuals, user <br className="sm:block hidden" />
             interfaces and web applications
           </p>
         </div>
@@ -27,19 +31,17 @@ const Hero = () => {
 
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+      <div className="absolute xs:bottom-10 bottom-0 w-full flex justify-center items-center z-10">
+        <a href="#about">
+          <div className="w-[35px] h-[50px] rounded-3xl !border-2 !border-[#b9b9ba] flex justify-center items-start p-2">
             <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
+              animate={{ y: [0, 24, 0] }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
                 repeatType: "loop",
               }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
+              className="w-3 h-3 rounded-full bg-[#b9b9ba] mb-1"
             />
           </div>
         </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}",
-    "./index.html",
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
-  mode: "jit",
-  important: "true",
   theme: {
     extend: {
       colors: {
