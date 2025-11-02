1d33baf9a130b37ff837389518ec1d9cb26921f9
Fidan-Eyyubova <fidaneyybva0@gmail.com>
2025-11-02 18:06:33 +0400

    Certificate

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Experience from './components/Experience'
 import Tech from './components/Tech'
-import Education from './components/Education'
 import Certificate from './components/Certificate'
+import Education from './components/Education'
 
 function App() {
 
